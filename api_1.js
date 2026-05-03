22773acac4b639b0b59e1fa45b5264e7d0471a1b
AnkitTEduz <ankit.thakur@eduztech.in>
2026-05-03 20:32:05 +0530

    updated api code

--- api_1.js
+++ api_1.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/",(req, res)=>{
-    res.send("Api is running")
+    res.send("Api is running by Ankit Thakur")
 });
 
 app.listen(PORT, () => {
